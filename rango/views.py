30e7af9e3bec8c87cda1b39ab0808eaeb65df52b
Andrea Tang <2960742Tstudent.gla.ac.uk>
2025-02-07 14:02:22 +0000

    completed the exercise for chapter 3

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 #this is one view
 def index(request):
     #book says that each view must return a HttpResponse object
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
+#chapter 3 exercise
+def about(request):
+    return HttpResponse("Rango says here is the about page <a href='/rango/'>Index</a>")
