6ddbefacff3d4504c771ee08f04a78909950b253
Andrea Tang <2960742Tstudent.gla.ac.uk>
2025-02-07 13:41:54 +0000

    second commit, mapped URLs and completed chapter 3.5

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from rango import views
 
 urlpatterns = [
     #maps url to the index function/view called index in views.py
     path('', views.index, name='index'),
+    #maps any URLs starting with rango/ to be handled by rango (pattern matching)
+    path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
+    
 ]
