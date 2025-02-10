37776314784346cc4e0dc1bd5a113558d0574808
Andrea Tang <2960742Tstudent.gla.ac.uk>
2025-02-10 10:13:05 +0000

    completed 5.7

--- rango/models.py
+++ rango/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
     class Meta:
-        verbose_name_plurals = 'Categories'
+        verbose_name_plural = 'Categories'
         
     def __str__(self):
         return self.name
